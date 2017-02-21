cb751d02c3189382852f386f7794dfa3b4bcc771
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-02-21 14:06:33 +0100

    Add navbar directive

---
+++ src/main/resources/static/javascript/directives.js
@@ -0,0 +1,11 @@
+angular.module('smartCampUZApp')
+
+    // include the 'navbar.html' into the <navbar> tag
+    .directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/navbar.html',
+            controller: 'navbarCtrl',
+            scope: {}
+        }
+    });
