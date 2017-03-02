d29a72d13d473afd8713a9d77d3fffef7dd7bfab
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-02 21:47:14 +0100

    Add auth flow onEnter states

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,8 +9,10 @@ angular.module('smartCampUZApp', ['ui.router', 'base64', 'angular-jwt'])
                 templateUrl: "templates/starter.html",
                 controller: "starterCtrl",
                 onEnter: function ($state, auth) {
-                    if (auth.isAuthenticated()) {
+                    if (auth.isAuthenticated() && (auth.getType() == 'admin')) {
                         $state.go('admin');
+                    } else if (auth.isAuthenticated() && (auth.getType() == 'maintenance')) {
+                        $state.go('maintenance');
                     }
                 }
             })
@@ -21,8 +23,10 @@ angular.module('smartCampUZApp', ['ui.router', 'base64', 'angular-jwt'])
                 templateUrl: "templates/admin.html",
                 controller: "adminCtrl",
                 onEnter: function ($state, auth) {
-                    if (!auth.isAuthenticated()) {
+                    if (!auth.isAuthenticated() || auth.isAuthenticated() && (auth.getType() == 'profesor')) {
                         $state.go('starter');
+                    } else if (auth.isAuthenticated() && (auth.getType() == 'maintenance')) {
+                        $state.go('maintenance');
                     }
                 }
             });
