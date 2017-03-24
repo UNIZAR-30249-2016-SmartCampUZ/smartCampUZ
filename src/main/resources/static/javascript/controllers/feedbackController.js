1c3a9b31f32210a8d88a45a27617411b2d80c4d2
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-24 17:49:41 +0100

    Added asign worker to feedback functionallity

--- src/main/resources/static/javascript/controllers/feedbackController.js
+++ src/main/resources/static/javascript/controllers/feedbackController.js
@@ -6,6 +6,10 @@ angular.module('smartCampUZApp')
         $scope.currentState = $scope.feedback.state;
         $scope.changeState = function (state) {
             $scope.currentState = state;
+            if ($scope.currentState == 'Aprobado' || $scope.currentState == 'Denegado'
+                || $scope.currentState == 'Notificado') {
+                $scope.selectedWorker = "";
+            }
             if ($scope.currentState != 'Asignado') {
                 var tmpState = {
                     id: $scope.feedback.id,
@@ -16,11 +20,13 @@ angular.module('smartCampUZApp')
             }
         };
         $scope.asignWorker = function () {
-            var tmpState = {
-                id: $scope.feedback.id,
-                state: $scope.currentState,
-                worker: $scope.selectedWorker
-            };
-            feedback.setState(tmpState);
+            if ($scope.currentState == 'Asignado') {
+                var tmpState = {
+                    id: $scope.feedback.id,
+                    state: $scope.currentState,
+                    worker: $scope.selectedWorker
+                };
+                feedback.setState(tmpState);
+            }
         }
     }]);
