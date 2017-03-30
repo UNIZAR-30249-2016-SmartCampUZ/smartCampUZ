333c30247b083c360f97367c727ebede63b70e6f
Catalin <catalindumitrache76@yahoo.es>
2017-03-30 18:11:49 +0200

    Leaflet integration with Geoserver

--- src/main/resources/static/javascript/controllers/mapController.js
+++ src/main/resources/static/javascript/controllers/mapController.js
@@ -80,6 +80,46 @@ angular.module('smartCampUZApp')
         		map.setView([41.68306, -0.88707], 17);
         	});
         	
+        	
+        	// Removes the respective layers
+        	var removeLayers = function (building, floor) {
+        		var Ada = ['00','01','02','03','04','S1','PT'];
+        		var Torres = ['00','01','02','03','S1'];
+        		var Betan = ['00','01','02','03','S1'];
+        		
+        		switch (building) {
+        		case 'A': 
+        			var n = Ada.indexOf(floor);
+        			for (i = 0; i < Ada.length; i++) {
+        			    if (i == n){}
+        			    else  {
+        			    	map.removeLayer(eval("A"+Ada[i]));
+        			    }
+        			}
+        			break;
+        		case 'T':
+        			var n = Torres.indexOf(floor);
+        			for (i = 0; i < Torres.length; i++) {
+        			    if (i == n){}
+        			    else  {
+        			    	map.removeLayer(eval("T"+Torres[i]));
+        			    }
+        			}
+        			break;
+        		case 'B':
+        			var n = Betan.indexOf(floor);
+        			for (i = 0; i < Betan.length; i++) {
+        			    if (i == n){}
+        			    else  {
+        			    	map.removeLayer(eval("B"+Betan[i]));
+        			    }
+        			}
+        			break;
+        		}
+        		
+        	};
+        	
+        	//Focuses on a building and changes floors
         	$scope.determineBuildingAndFloor = function(buildingAndFloor) {
         		
         		var building = buildingAndFloor.substring(0,1);
@@ -89,12 +129,91 @@ angular.module('smartCampUZApp')
             		$scope.floors[0]=floor;
             		map.setView([41.68363, -0.88891], 19);
             		
+            		switch (floor) {
+            	    case '00':
+            	        removeLayers('A','00');
+            	        A00.addTo(map);
+            	        break;
+            	    case '01':
+            	        removeLayers('A','01');
+            	        A01.addTo(map);
+            	        break;
+            	    case '02':
+            	    	removeLayers('A','02');
+            	        A02.addTo(map);
+            	        break;
+            	    case '03':
+            	        removeLayers('A','03');
+            	        A03.addTo(map);
+            	        break;
+            	    case '04':
+            	        removeLayers('A','04');
+            	        A04.addTo(map);
+            	        break;
+            	    case 'S1':
+            	        removeLayers('A','S1');
+            	        AS1.addTo(map);
+            	        break;
+            	    case 'PT':
+            	        removeLayers('A','PT');
+            	        APT.addTo(map);
+            	        break;
+            		} 
+            		
             	}else if (building=='T'){
             		$scope.floors[1]=floor;
             		map.setView([41.68363, -0.88736], 19);
+            		
+            		switch (floor) {
+            	    case '00':
+            	        removeLayers('T','00');
+            	        T00.addTo(map);
+            	        break;
+            	    case '01':
+            	        removeLayers('T','01');
+            	        T01.addTo(map);
+            	        break;
+            	    case '02':
+            	    	removeLayers('T','02');
+            	        T02.addTo(map);
+            	        break;
+            	    case '03':
+            	        removeLayers('T','03');
+            	        T03.addTo(map);
+            	        break;
+            	    case 'S1':
+            	        removeLayers('T','S1');
+            	        TS1.addTo(map);
+            	        break;
+            		} 
+            		
+            		
             	}else{
             		$scope.floors[2]=floor;
             		map.setView([41.68347, -0.88394], 19);
+            		
+            		switch (floor) {
+            	    case '00':
+            	        removeLayers('B','00');
+            	        B00.addTo(map);
+            	        break;
+            	    case '01':
+            	        removeLayers('B','01');
+            	        B01.addTo(map);
+            	        break;
+            	    case '02':
+            	    	removeLayers('B','02');
+            	        B02.addTo(map);
+            	        break;
+            	    case '03':
+            	        removeLayers('B','03');
+            	        B03.addTo(map);
+            	        break;
+            	    case 'S1':
+            	        removeLayers('B','S1');
+            	        BS1.addTo(map);
+            	        break;
+            		} 
             	}
         	};
         	
@@ -109,8 +228,21 @@ angular.module('smartCampUZApp')
         	    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         	    id: 'mapbox.streets'
         	}).addTo(map);
+
+        	var AS1 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:AS1',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
         	 	
-        	var PoligonosAda00 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        	var A00 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
         		request: 'GetMap',
         		maxZoom: 25,
         		layers: 'Smart_CampUZ:A00',
@@ -123,10 +255,88 @@ angular.module('smartCampUZApp')
         		zIndex: 2
         	}).addTo(map);
 
-        	var PoligonosAda04 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        	var A01 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
         		request: 'GetMap',
         		maxZoom: 25,
-        		layers: 'Smart_CampUZ:PoligonosAda04',
+        		layers: 'Smart_CampUZ:A01',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var A02 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:A02',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var A03 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:A03',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+        	
+        	var A04 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:A04',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var APT = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:APT',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var BS1 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:BS1',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+        	 	
+        	var B00 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:B00',
         	    noWrap:true,
         	    continuousWorld: false,
         	    transparent: true,
@@ -136,11 +346,115 @@ angular.module('smartCampUZApp')
         		zIndex: 2
         	}).addTo(map);
 
+        	var B01 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:B01',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
 
-        	var overlaysAda = {
-        		    "Ada Byron P00": PoligonosAda00,
-        		    "Ada Byron P04": PoligonosAda04
-        	};
-        	L.control.layers(overlaysAda).addTo(map);
+        	var B02 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:B02',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var B03 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:B03',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+        	
+        	var TS1 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:TS1',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+        	 	
+        	var T00 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:T00',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	}).addTo(map);
+
+        	var T01 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:T01',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var T02 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:T02',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+        	var T03 = L.tileLayer.wms("http://localhost:8080/geoserver/wms",{
+        		request: 'GetMap',
+        		maxZoom: 25,
+        		layers: 'Smart_CampUZ:T03',
+        	    noWrap:true,
+        	    continuousWorld: false,
+        	    transparent: true,
+        	    format: 'image/png',
+        	    version: '1.1.0',
+        	    attribution: "myattribution",
+        		zIndex: 2
+        	});
+
+
+//        	var overlaysAda = {
+//        		    "Ada Byron P00": PoligonosAda00,
+//        		    "Ada Byron P04": PoligonosAda04
+//        	};
+//        	L.control.layers(overlaysAda).addTo(map);
         });
     }]);
