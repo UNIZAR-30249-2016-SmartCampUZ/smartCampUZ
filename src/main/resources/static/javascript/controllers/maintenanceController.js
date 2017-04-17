141c46c8e5c32e90b9198b43864b19bdba9b1d25
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-04-17 19:22:20 +0200

    Added get list of reports assigned to a worker

--- src/main/resources/static/javascript/controllers/maintenanceController.js
+++ src/main/resources/static/javascript/controllers/maintenanceController.js
@@ -1,5 +1,31 @@
 angular.module('smartCampUZApp')
 
-    .controller('maintenanceCtrl', ['$scope', function ($scope) {
+    .controller('maintenanceCtrl', ['$scope', 'maintenance', 'userMap', 'Notification',
+        function ($scope, maintenance, userMap, Notification) {
+        $scope.reportsList = [];
+
+        /* FEEDBACK MESSAGES */
+        // show the error message
+        var showError = function (message) {
+            Notification.error('&#10008' + message);
+        };
+
+        // show the success message
+        var showSuccess = function (message) {
+            Notification.success('&#10004' + message);
+        };
+
+        maintenance.getReports(function (list) {
+            $scope.reportsList = list;
+        }, showError);
+
+        // Watches to control if the user have selected a location
+        $scope.$watch(function() {
+            return userMap.getCurrentLocation();
+        }, function () {
+            maintenance.getReports(function (list) {
+                $scope.reportsList = list;
+            }, showError);
+        });
 
     }]);
